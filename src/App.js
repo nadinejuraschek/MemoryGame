e659043a46cc9f9302993abc300230aa42ebeea6
Nadine Ju <nadine.jura@online.de>
2020-02-12 21:14:37 -0800

    added wrapper component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import React from "react";
+import React, { Component } from "react";
+import Wrapper from "./components/Wrapper";
 
-function App() {
-  return (
-    <div>Hello
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Wrapper />
+    );
+  }
 }
 
 export default App;
