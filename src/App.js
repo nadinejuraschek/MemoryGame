5decd131a6023c11f8151456bf280eee507b6967
Nadine Ju <nadine.jura@online.de>
2020-02-12 21:26:33 -0800

    first card component rendered

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import Wrapper from "./components/Wrapper";
 import Header from "./components/Header";
+import Card from "./components/Card";
+import cards from "./cards.json";
 
 class App extends Component {
   // Setting this.state.cards to the cards json array
@@ -14,6 +16,7 @@ class App extends Component {
     return (
       <Wrapper>
         <Header score={this.state.score} highscore={this.state.highscore}>Clicky Game</Header>
+        <Card />
       </Wrapper>
     );
   }
