56182d17f072e77c3c5fa5baa769488d9d78cac1
Nadine Ju <nadine.jura@online.de>
2020-02-12 21:21:04 -0800

    CSS for Header component added

--- src/App.js
+++ src/App.js
@@ -5,11 +5,11 @@ import Header from "./components/Header";
 class App extends Component {
   // Setting this.state.cards to the cards json array
   state = {
-    cards,
+    // cards,
     score: 0,
     highscore: 0
   };
-  
+
   render() {
     return (
       <Wrapper>
