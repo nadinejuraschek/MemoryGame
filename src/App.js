5949ff3013eb23fe29bb67016cd3ac640fddc54a
Nadine Ju <nadine.jura@online.de>
2020-02-13 09:35:24 -0800

    Game works, cards display

--- src/App.js
+++ src/App.js
@@ -1,22 +1,65 @@
 import React, { Component } from "react";
+import Card from "./components/Card";
 import Wrapper from "./components/Wrapper";
 import Header from "./components/Header";
-import Card from "./components/Card";
+import Footer from "./components/Footer";
 import cards from "./cards.json";
 
 class App extends Component {
-  // Setting this.state.cards to the cards json array
+
   state = {
-    // cards,
+    cards,
     score: 0,
     highscore: 0
   };
+
+  // end game
+  gameOver = () => {
+    // set highscore
+    if (this.state.score > this.state.highscore) {
+      this.setState({highscore: this.state.score}, function() {
+        console.log(this.state.highscore);
+      });
+    }
+    this.state.cards.forEach(card => {
+      card.count = 0;
+    });
+    // game over message
+    alert(`Game Over :( \nscore: ${this.state.score}`);
+    this.setState({score: 0});
+    return true;
+  }
+
+  clickCount = id => {
+    this.state.cards.find((o, i) => {
+      if (o.id === id) {
+        if(cards[i].count === 0){
+          cards[i].count = cards[i].count + 1;
+          this.setState({score : this.state.score + 1}, function(){
+            console.log(this.state.score);
+          });
+          this.state.cards.sort(() => Math.random() - 0.5)
+          return true; 
+        } else {
+          this.gameOver();
+        }
+      }
+    });
+  }
 
   render() {
     return (
       <Wrapper>
         <Header score={this.state.score} highscore={this.state.highscore}>Clicky Game</Header>
-        <Card />
+        {this.state.cards.map(card => (
+          <Card
+            clickCount={this.clickCount}
+            id={card.id}
+            key={card.id}
+            image={card.image}
+          />
+        ))}
+        <Footer />
       </Wrapper>
     );
   }
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = props => (
+    <div className='footer'>
+        <p>Icons by <a href="https://www.flaticon.com/">Flaticon</a> and <a href="https://www.freepik.com/">Freepik</a>.</p>
+    </div>
+);
+
+export default Footer;
