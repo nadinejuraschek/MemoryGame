fbd05b7a8f19de5996ca886703e59d214beae746
Nadine Ju <nadine.jura@online.de>
2020-03-30 21:01:20 -0700

    footer edited

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './Footer.css';
 
+const currentYear = new Date().getFullYear();
+
 const Footer = props => (
-    <div className='footer'>
-        <p>Icons by <a href="https://www.flaticon.com/">Flaticon</a> and <a href="https://www.freepik.com/">Freepik</a>.</p>
-    </div>
+        <div className='footer'>
+            <p>© { currentYear } <a href="http://nadinejuraschek.com/">Nadine Juraschek</a></p>
+            <p>Icons by <a href="https://www.flaticon.com/">Flaticon</a> and <a href="https://www.freepik.com/">Freepik</a>.</p>
+        </div>
 );
 
 export default Footer;
